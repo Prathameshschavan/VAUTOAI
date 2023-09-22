99d192767b70fb720bb4879fcdc91a2a386e01a5
Prathamesh Shashikant Chavan <117484138+Prathameshschavan@users.noreply.github.com>
2023-09-22 22:56:49 +0530

    Delete backend directory

--- backend/routes/outward.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addOutward, getOutward } from "../controller/outward.controller.js";
-
-const router = express.Router();
-
-router.get("/", getOutward);
-router.post("/out", addOutward );
-
-export default router;
