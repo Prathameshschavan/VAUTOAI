f5ee0e9b4118ea8c3c5cb5056407efb0a81a13ba
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2023-10-05 23:44:34 +0530

    master in progress

---
+++ routes/master.route.js
@@ -0,0 +1,38 @@
+import express from "express";
+const router = express.Router();
+
+router.post("/add",);
+
+export default router;
+/*
+
+
+floor:1
+departments:[
+IT:{
+    id:"",
+    headName:"",
+    heademail:"",
+    headNumber:"",
+    emplyecount:""
+},
+HR:{
+    id:"",
+    headName:"",
+    heademail:"",
+    headNumber:"",
+    emplyecount:""
+},
+VP:{
+    id:"",
+    headName:"",
+    heademail:"",
+    headNumber:"",
+    emplyecount:""
+}
+
+]
+
+
+
+*/
